9beac92249732af107f43a6c3303d754860c4174
Daria <lara-lorn@yandex.ru>
2023-02-25 15:55:51 +0400

    saving program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите текст через запятую: "); 
+string arr = Console.ReadLine()!; 
+string[] num = arr.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries); 
+int size = 3;  
+for (int i = 0; i < num.Length; i++) 
+{ 
+    if (num[i].Length <= size) 
+    { 
+        Console.WriteLine(num[i]);  
+    } 
+}
